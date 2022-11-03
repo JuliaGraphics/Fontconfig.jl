4a3e10530154aa489df66397d70c77abc083fded
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-11-03 22:14:08 +0530

    Customize properties returned in `list`
    This is useful for e.g. Freetype integration, where one may want to load a list object directly into a FreeType font - this necessitates either the index of the font or its direct `ftface` parameter.  This change allows both.

--- src/Fontconfig.jl
+++ src/Fontconfig.jl
@@ -132,7 +132,13 @@ struct FcFontSet
 end
 
 
-function list(pat::Pattern=Pattern())
+"""
+    list(pat::Pattern=Pattern(), properties = ["family", "style", "file"])::Vector{Pattern}
+    
+Selects fonts matching `pat` and creates patterns from those fonts. These patterns containing only those 
+properties listed in `properties`, and returns a vector of unique such patterns, as a `Vector{Pattern}`.
+"""
+function list(pat::Pattern=Pattern(), properties = ["family", "style", "file"])
     os = ccall((:FcObjectSetCreate, libfontconfig), Ptr{Nothing}, ())
     ccall((:FcObjectSetAdd, libfontconfig), Cint, (Ptr{Nothing}, Ptr{UInt8}),
           os, "family")
